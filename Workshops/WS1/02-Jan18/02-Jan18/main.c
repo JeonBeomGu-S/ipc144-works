232cdb582469b6488ebf125ba236b6979dfb91e3
Beomgu <jb98967@gmail.com>
2024-01-18 10:35:31 -0500

    2024.01.18 Study 1
    Practice int and double variable

---
+++ Workshops/WS1/02-Jan18/02-Jan18/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  02-Jan18
+//
+//  Created by 전범구 on 2024-01-18.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    // insert code here...
+    int inum;
+    double dnum;
+    
+    inum = 12345;
+    dnum = 1234.5678;
+    
+    printf("Integer value: %d, Double value: %lf\n", inum, dnum);
+    
+    return 0;
+}
