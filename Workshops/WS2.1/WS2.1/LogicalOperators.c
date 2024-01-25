639659433485f0da257345bd351ab6bcb2091be5
Beomgu <jb98967@gmail.com>
2024-01-25 09:57:27 -0500

    2024.01.23 study

---
+++ Workshops/WS2.1/WS2.1/LogicalOperators.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  WS2.1
+//
+//  Created by 전범구 on 2024-01-23.
+//
+
+/* logical operatiors always return 1 or 0
+ ==
+ !=
+ >
+ <
+ >=
+ <=
+ !
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void name(void);
+
+//int main(void) {
+//    int x, y;
+//    int b = 10;
+//    int c = 20;
+//    name();
+//    
+//    printf("having b as %d and c as %d\n", b, c);
+//    x = b > c;
+//    printf("b > c is %d\n", x);
+//    x = b < c;
+//    printf("b < c is %d\n", x);
+//    x = b == c;
+//    printf("b == c is %d\n", x);
+//    x = b != c;
+//    printf("b != c is %d\n", x);
+//    // anything but zero is true
+//    x = !c;
+//    printf("!c is %d\n", x);
+//    
+//    c = !x;
+//    printf("x being zero!\n");
+//    printf("!x is %d\n", c);
+//    return 0;
+//}
