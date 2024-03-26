243e62d456d6564755be3c3448c1c08e50a06cca
Beomgu <jb98967@gmail.com>
2024-03-26 10:21:41 -0400

    Initialize Project

---
+++ Project/MS4/MS4/main.c
@@ -0,0 +1,17 @@
+//	
+//	main.c
+//	MS4
+//	Name: Beomgu Jeon
+//	Email: bgjeon@myseneca.ca
+//	StudentID: 126428234
+//	Date: 2024-03-26
+//	
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
